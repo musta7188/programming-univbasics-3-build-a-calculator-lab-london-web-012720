fc885c04a8d10b0fc53e9d09eee95face1abd70a
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-22 15:31:28 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,16 +1,22 @@
 def addition(num1, num2)
 
+add = num1 + num2
+
 end
 
 def subtraction(num1, num2)
+  subtr = num1 - num2
 
 end
 
 def division(num1, num2)
 
+div = num1 / num2
 end
 
 def multiplication(num1, num2)
+
+  mult = num1 * num2
 
 end
 
